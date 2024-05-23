a5b252fdd6da194e5a090ed59af6a9f5f7319757
sjeevankumar <sjeevankumar89@gmail.com>
2024-05-23 11:11:41 +0530

    feat:solve third problem in functions concept

---
+++ 05_functions/03_solution.py
@@ -0,0 +1,8 @@
+# Polymorphism in Functions
+# Write a function multiply that multiplies two numbers, but can also accept and multiply strings
+
+def calculate_multiplication(p1,p2):
+    return p1 * p2
+
+result = calculate_multiplication(8,'j')
+print(result)
