0bbaa5aa500c7fea168fb58bf3a3523f0d94033b
sjeevankumar <sjeevankumar89@gmail.com>
2024-06-03 10:17:29 +0530

    feat:complete error handling in python

---
+++ 09_error_handling/youtube_manager.py
@@ -0,0 +1,85 @@
+import json
+
+def add_decoration(message):
+    print('\n')
+    print('*' * 70)
+    print(message)
+    print('*' * 70)
+    
+def load_data():
+    try:
+        with open('youtube.txt','r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data_helper(videos):
+    with open('youtube.txt','w') as file:
+        json.dump(videos,file)
+
+def list_all_videos(videos):
+    print('*'*70)
+    
+    for index,video in enumerate(videos,start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']}")
+
+    print('*'*70)
+    
+def add_video(videos):
+    name = input('Enter video name: ')
+    time = input('Enter video time: ')
+    videos.append({'name':name,'time':time})
+    save_data_helper(videos)
+    add_decoration('video successfully added')
+    
+def update_video(videos):
+    list_all_videos(videos)
+    index = int(input('Enter the video number to update: '))
+    if index>=1 and index<=len(videos):
+        name = input('Enter the new video name: ')
+        time = input('Enter the new video time: ')
+        videos[index-1] = {'name':name,'time':time}
+        save_data_helper(videos)
+        add_decoration('video successfully updated')
+    else:
+        print('invalid video index selected')
+
+def delete_video(videos):
+    list_all_videos(videos)
+    
+    index = int(input('Enter the video number to be deleted: '))
+    if index>=1 and index<=len(videos):
+        del videos[index - 1]
+        save_data_helper(videos)
+        add_decoration('video successfully deleted')
+    else:
+        print('invalid video index selected')
+
+def main():
+    videos = load_data()
+    while True:
+        print('\n Youtube Manager | choose an option')
+        print('1. List all youtube videos ')
+        print('2. Add a youtube video ')
+        print('3. Update a youtube video details ')
+        print('4. Delete a youtube video ')
+        print('5. Exit the app ')
+        
+        choice = input('Enter your choice: ')
+        match choice:
+            case '1':
+                list_all_videos(videos)
+            case '2':
+                add_video(videos)
+            case '3':
+                update_video(videos)
+            case '4':
+                delete_video(videos)
+            case '5':
+                break
+            case _:
+                print('invalid choice')
+
+if __name__ == '__main__':
+    main()
+            
